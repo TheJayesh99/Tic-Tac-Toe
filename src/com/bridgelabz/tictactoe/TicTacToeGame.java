c1ced7380f060b7f4c94ff7855d6ae1bece7ce35
TheJayesh99 <jayeshm.official@gmail.com>
2021-06-25 09:20:04 +0530

    Modified: Removed Unwanted spaces

--- src/com/bridgelabz/tictactoe/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoe/TicTacToeGame.java
@@ -4,17 +4,13 @@ package com.bridgelabz.tictactoe;
 
 public class TicTacToeGame {
 	public static void main(String[] args) {
-		
-		char[] board = new char[10];
-		
-		createBoard(board);
-		
+		createBoard();
 	}
 
-	private static void createBoard(char[] board) {
+	private static void createBoard() {
+		char[] board = new char[10];
 		for (int i = 1; i < board.length; i++) {
 			board[i] = '-';
-		
 		}
 	}
 }
