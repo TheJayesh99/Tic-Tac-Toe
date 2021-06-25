32efc85edcaf9e89b8820837ca98aa15ca08d4b5
TheJayesh99 <jayeshm.official@gmail.com>
2021-06-25 10:22:37 +0530

    Added : Method to asked player to input Symbol and accordingly set computers symbol

--- src/com/bridgelabz/tictactoe/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoe/TicTacToeGame.java
@@ -1,16 +1,45 @@
 package com.bridgelabz.tictactoe;
 
+import java.util.Scanner;
 
-
-public class TicTacToeGame {
-	public static void main(String[] args) {
-		createBoard();
+public class TicTacToeGame
+{
+	public static void main(String[] args)
+	{
+		Scanner scanner = new Scanner(System.in);		
+		char[] board = createBoard();
+		System.out.println("Enter The Sysmbol which you want i.e. X or O");
+		char playerSymbol = scanner.next().charAt(0);
+		scanner.close();
+		char computerSymbol = determineComputerSymbol(playerSymbol);
 	}
 
-	private static void createBoard() {
+	private static char determineComputerSymbol(char playerSymbol) {
+		char computerSymbol = ' ' ;
+		if (playerSymbol == 'X')
+		{
+			 computerSymbol = 'O';
+		}
+		else if (playerSymbol == 'O')
+		{
+			 computerSymbol = 'X';
+		}
+		else
+		{
+			System.err.println("You inserted a wrong Symbol");
+		}
+		return computerSymbol;
+	}
+
+	private static char[] createBoard()
+	{
 		char[] board = new char[10];
-		for (int i = 1; i < board.length; i++) {
+		for (int i = 1; i < board.length; i++) 
+		{
 			board[i] = '-';
 		}
+		return board;
 	}
+
+	
 }
