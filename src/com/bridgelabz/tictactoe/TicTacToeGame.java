f196c775e8680b15a14242bd7047ffa2e4bbf471
TheJayesh99 <jayeshm.official@gmail.com>
2021-06-27 11:55:12 +0530

    Added : Game continuation till games overs

--- src/com/bridgelabz/tictactoe/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoe/TicTacToeGame.java
@@ -23,12 +23,13 @@ public class TicTacToeGame
 			{
 				if(playerSymbol == winner)
 				{					
-					System.out.println("Player Won");
+					System.out.println("Player Won ");
 				}
 				else
 				{
 					System.out.println("Computer Won");
 				}
+				System.out.println("Game Over");
 				break;
 			}
 			else if(checkFreeSpace(board))
@@ -49,7 +50,7 @@ public class TicTacToeGame
 			}
 			else
 			{
-				System.out.println("The game Tie");
+				System.out.println("The game Tie \n Game Over");
 				break;
 
 			}
